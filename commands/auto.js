380c00e09c932b9bd193ded95d49e1faeb4e2df8
Andy2306 <72691938+Andy2306@users.noreply.github.com>
2024-02-28 22:53:27 +0700

    Update auto.js
    change code

--- commands/auto.js
+++ commands/auto.js
@@ -1,22 +1,7 @@
 module.exports = [
     {
-        name: "rules",
+        name: "ping",
         code: `
-        $title[RULES!]
-        $color[$userRoleColor[$authorID;$guildID]]
-        $description[HARAP PATUHI PERTURAN BERIKUT!
-        > **1.Patuhi discord guide lines!**
-        > **2.Hormati seluruh member yang ada disini!**
-        > **3.Dilarang memposting konten yang berisi NSFW (18+).**
-        > **4.Dilarang mengirim promosi diri, spam, dan tautan yang tidak relevan.**
-        > **5.Diwajibkan untuk mematuhi Discord Term of Service ( https://discord.com/terms ).**
-        > **6.Admin Berhak melakukan Timeout/Kick/Ban disetiap pelanggarannya.**
-        
-        HAL YANG HARUS DIPERHATIKAN!
-        > **1.Untuk mendapatakan role harap hubungi admin berikut:**
-        > <@1095999050689552404>
-        > <@998922497288323083>
-        > <@708981391630532689>]
-        $footer[$guildName[$guildID]]`,
+        $ping`,
     },
 ]
